c297c30778ca8ebfd52a16019a4ff0067a44f09b
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-05-07 20:28:31 +1000

    Added a basic viewmodel, no actual data

---
+++ A3/app/src/main/java/com/example/assignment3/HealthViewModel/HealthViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.assignment3.HealthViewModel
+
+import androidx.lifecycle.ViewModel
+import java.util.Date
+
+class HealthViewModel: ViewModel() {
+
+    // Business logic
+
+    // Get user information from the store
+    fun getUserInformation(){
+        return
+    }
+
+    // Create booking function.
+    // @params: Booking date, User Id, Doctor, Clinic Location
+    // This will take in booking date as timestamp? user id for identification,\
+    // doctor id for identification and clinic location id.
+    fun scheduleBooking(date: Any, userId: String, doctorId: Any, clinicIid: Any) {
+        return
+    }
+}
