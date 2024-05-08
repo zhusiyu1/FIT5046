e9062146f59e19d57b3a9b6f1c0d676cde950a3e
zhusiyu1 <413417417@qq.com>
2024-05-09 00:28:59 +1000

    google login

--- A3/app/src/main/java/com/example/assignment3/Login.kt
+++ A3/app/src/main/java/com/example/assignment3/Login.kt
@@ -2,20 +2,15 @@ package com.example.assignment3
 
 import android.content.ContentValues.TAG
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,33 +20,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
 
 
 @Composable
 fun Login(navController: NavController) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var isValid by remember { mutableStateOf(false) }
-
+    var isEmailValid by remember { mutableStateOf(false) }
+    var isPassValid by remember { mutableStateOf(false) }
+    val token = stringResource(R.string.client_id)
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -73,33 +61,47 @@ fun Login(navController: NavController) {
         OutlinedTextField(
             value = email,
             onValueChange = { email = it
-                isValid = it.isNotEmpty()},
+                isEmailValid = it.isNotEmpty()},
             label = { Text("Email") },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 270.dp),
             keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
-            isError = !isValid
+            isError = !isEmailValid
         )
+
+
 
         OutlinedTextField(
             value = password,
             onValueChange = { password = it
-                isValid = it.isNotEmpty()},
+                isPassValid = it.isNotEmpty()},
             label = { Text("Password") },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 350.dp),
             keyboardOptions = KeyboardOptions.Default.copy(),
-            isError = !isValid
+            isError = !isPassValid
         )
 
-        if (!isValid) {
-            Text(text = "The input cannot be empty", color = Color.Red)
+        Column {
+            if (!isEmailValid) {
+                Text(text = "The email cannot be empty", color = Color.Red, modifier = Modifier.padding(start = 16.dp))
+            }
+            if (!isPassValid) {
+                Text(text = "The password cannot be empty", color = Color.Red, modifier = Modifier.padding(start = 16.dp))
+            }
         }
 
+
+
         Button(
-            onClick = { },
+            onClick = {
+               val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(token)
+                    .requestEmail()
+                    .build()
+            },
             modifier = Modifier
                 .padding(end = 16.dp, bottom = 300.dp)
                 .align(Alignment.BottomEnd)
